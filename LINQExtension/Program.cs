56d9590ea9a7af406de8688280e97b76d3ab689c
ggnadirashvilii <ggnadirashvilii@gmail.com>
2024-02-07 20:07:03 +0400

    Create int and string Lists, Concatenate and remove dublications.

---
+++ LINQExtension/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace LINQExtension
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var random = new Random();
+            int iteration = 1000;
+
+            // Create List with ints
+            List<int> intList = new List<int>();
+
+            for (int i = 0; i < iteration; i++)
+            {
+                int randomNumbers = random.Next(100, 1000);
+                intList.Add(randomNumbers);
+            }
+
+            // Create List with strings
+
+            List<string> stringList = new List<string>();
+
+            for (int i = 0; i < iteration; i++)
+            {
+                int randomNumbers = random.Next(100, 1000);
+                stringList.Add(randomNumbers.ToString());
+            }
+
+            // Concatenate the two lists
+            List<string> concatenatedList = new List<string>();
+
+            foreach (var num in intList)
+            {
+                concatenatedList.Add(num.ToString());
+            }
+
+            // Add all elements from stringList to concatenatedList
+            concatenatedList.AddRange(stringList);
+
+            // Remove dublications
+            var removedDublicates = concatenatedList.Distinct().ToList();
+
+            Console.WriteLine($"There is {removedDublicates.Count()} duplication in the List");
+
+        }
+    }
+}
