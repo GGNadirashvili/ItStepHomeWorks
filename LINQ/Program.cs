130483aba2603f7bc3ff1e196cde9d6829f8006e
ggnadirashvilii <ggnadirashvilii@gmail.com>
2024-02-07 19:15:31 +0400

    GET Palindrome Numbers using LINQ

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -4,7 +4,33 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var random = new Random();
+            var numbers = Enumerable.Range(0, 1000000)
+                                    .Select(_ => random.Next(10000, 99999));
+
+            var palindromeNumbers = numbers
+                .Where(n => IsPalindrome(n.ToString()));
+
+            int palindromeCount = palindromeNumbers.Count();
+
+            if (palindromeNumbers.Any())
+            {
+                Console.WriteLine("PalindromeNumbers");
+                foreach (var palindrome in palindromeNumbers)
+                {
+                    Console.WriteLine(palindrome);
+                }
+                Console.WriteLine("Count:" + " " + palindromeCount);
+            }
+            else
+            {
+                Console.WriteLine("No palindrome numbers found.");
+                Console.WriteLine("Count:" + " " + palindromeCount);
+            }
+        }
+        static bool IsPalindrome(string s)
+        {
+            return s.SequenceEqual(s.Reverse());
         }
     }
 }
