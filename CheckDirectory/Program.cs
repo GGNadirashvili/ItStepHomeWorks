30a0b7e6ce36817a267fc10d984e373bc1581bff
ggnadirashvilii <ggnadirashvilii@gmail.com>
2024-02-13 23:23:10 +0400

    Check Folders & Files using Recursion

---
+++ CheckDirectory/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        string path = @"C:\Program Files";
+        PrintDirectoryContents(path);
+    }
+
+    static void PrintDirectoryContents(string path)
+    {
+        try
+        {
+            Console.WriteLine($"Directory: {path}");
+
+            string[] files = Directory.GetFiles(path);
+            foreach (string file in files)
+            {
+                Console.WriteLine($"File: {file}");
+            }
+
+            string[] subDirectories = Directory.GetDirectories(path);
+            foreach (string subDirectory in subDirectories)
+            {
+                PrintDirectoryContents(subDirectory);
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+    }
+}
