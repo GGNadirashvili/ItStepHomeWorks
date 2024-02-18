2a7f6c7a5953769831043be6516ee7dc5db59d41
ggnadirashvilii <ggnadirashvilii@gmail.com>
2024-02-18 13:55:45 +0400

    Serialization & Deserialization

---
+++ SerializationAndDeserialization/Program.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+
+namespace SerializationAndDeserialization
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MySerializer serializer = new MySerializer();
+
+            var obj = new { Name = "Giorgi", Age = 23 };
+            string json = serializer.Serialize(obj);
+            Console.WriteLine("Serialized JSON:");
+            Console.WriteLine(json);
+
+            var newObj = serializer.Deserialize<dynamic>(json);
+            Console.WriteLine("\nDeserialized Object:");
+            Console.WriteLine($"Name: {newObj.Name}, Age: {newObj.Age}");
+        }
+    }
+
+        public class MySerializer
+        {
+            public string Serialize(object obj)
+            {
+                return JsonConvert.SerializeObject(obj);
+            }
+
+            public T Deserialize<T>(string json)
+            {
+                return JsonConvert.DeserializeObject<T>(json)!;
+            }
+        }
+}
